da58e6b16d3d68c2894ee4492b7a5e0adf2b36a3
Marthym <marthym@gmail.com>
2022-03-19 15:36:28 +0100

    fix(core): remove useless classes

--- scraphead-core/src/main/java/fr/ght1pc9kc/scraphead/core/model/opengraph/OpenGraphException.java
+++
@@ -1,7 +0,0 @@
-package fr.ght1pc9kc.scraphead.core.model.opengraph;
-
-public final class OpenGraphException extends RuntimeException {
-    public OpenGraphException(Throwable e) {
-        super(e);
-    }
-}
